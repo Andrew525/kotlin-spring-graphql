d7a3c753d0be434d14e329954544d0e86f1c49bb
andriipod <andrij525@gmail.com>
2023-06-26 18:27:25 -0400

    fix imports

--- src/main/kotlin/com/example/kotlindemo/repository/AuthorRepository.kt
+++ src/main/kotlin/com/example/kotlindemo/repository/AuthorRepository.kt
@@ -2,19 +2,17 @@ package com.example.kotlindemo.repository
 
 import com.example.kotlindemo.model.Author
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
-import java.util.ArrayList
 
 @Repository
 class AuthorRepository {
 
-    fun findById(authorId: Int): Author {
-        println("Find By author id: $authorId")
-        return AUTHOR[authorId] ?: throw RuntimeException("Not found with id $authorId")
-    }
+//    fun findById(authorId: Int): Author {
+//        println("Find By author id: $authorId")
+//        return AUTHOR[authorId] ?: throw RuntimeException("Not found with id $authorId")
+//    }
 
     fun findAlByIdsAsMap(ids: Set<Int>): Map<Int, Author> {
-        return Companion.AUTHOR.filterKeys{ it in ids }
+        return AUTHOR.filterKeys{ it in ids }
             .also { println("Find All Authors by Ids: $ids") }
     }
     private companion object {
--- src/main/kotlin/com/example/kotlindemo/web/BookQuery.kt
+++ src/main/kotlin/com/example/kotlindemo/web/BookQuery.kt
@@ -7,7 +7,6 @@ import com.example.kotlindemo.repository.BookRepository
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.BatchMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
-import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Mono
 
